ebaaa758676089724fb5f4dbcf9e9a651ba98869
Gana <gananathanthirunavukkarasu@gmail.com>
2022-09-27 10:28:23 +0530

    crud api

---
+++ index.js
@@ -0,0 +1,63 @@
+// to import the express using the import module add module to the package.json
+import express from "express";
+import { MongoClient } from "mongodb";
+
+// calling the express function to use the methods in it
+const app = express();
+
+// the data is not json still node js will do the work for us
+const data = [
+  {
+    id: 1,
+    name: "gana",
+  },
+  {
+    id: 2,
+    name: "ishu",
+  },
+  {
+    id: 3,
+    name: "anjali",
+  },
+];
+
+const MONGO_URL = "mongodb://localhost";
+// hvHqysPyr0j62CDG
+app.use(express.json());
+async function createconnection(){
+const client = new MongoClient(MONGO_URL);
+await client.connect();
+console.log("mongodb connected");
+return client;
+}
+
+const client = await createconnection();
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+app.get("/data", function (req, res) {
+  res.send(data);
+});
+
+app.get("/data/:id",async function (req, res) {
+  console.log(req.params);
+  const { id } = req.params;
+  // this below line displays the data in an array
+  // const single_data = data.filter((sd)=>sd.id==id)
+  // this below line with the first index looks hacky
+  // const single_data = data.filter((sd)=>sd.id==id)[0]
+  // so we use another method similar to filter which is find, find only return an element whereas
+  //  filter returns an array
+  // const single_data = data.find((sd) => sd.id == id);
+  const single_data = await client.db("crud").collection("data").findOne({id:id})
+  single_data ?  res.send(single_data) : res.status(404).send({msg:"data not found"});
+});
+
+
+
+const port = 4000;
+app.listen(port, () => {
+  console.log(`app started in ${port}`);
+});
